4fc5b01f1af68a918c0cb199dcb7c4450fbcae8a
Mirsaid Abdullaev <abdullaevm017@gmail.com>
2024-05-16 15:34:55 +0100

    Finished MergeSorter, added CSVManager class

---
+++ CSVManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DSA
+{
+    internal static class CSVManager
+    {
+        public static string[] ParseFile(string FilePath) //returns all the rows of the csv file
+        {
+            if (Path.GetExtension(FilePath) != ".csv")
+            {
+                throw new FormatException("Error: file specified is not a CSV");
+            }
+            if (!File.Exists(FilePath))
+            {
+                throw new FileNotFoundException("Error: no such file exists.");
+            }
+
+            //at this point, file exists and is a csv file
+            string[] FullData;
+            using (StreamReader reader = new StreamReader(FilePath))
+            {
+                try
+                {
+                    FullData = reader.ReadToEnd().Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return FullData; //returns all not-null data rows
+        }
+
+        public static double[][] ParseFileDouble(string FilePath)
+        {
+            if (Path.GetExtension(FilePath) != ".csv")
+            {
+                throw new FormatException("Error: file specified is not a CSV");
+            }
+            if (!File.Exists(FilePath))
+            {
+                throw new FileNotFoundException("Error: no such file exists.");
+            }
+
+            // At this point, file exists and is a csv file
+            string[] FullData;
+            double[][] ParsedData;
+            using (StreamReader reader = new StreamReader(FilePath))
+            {
+                try
+                {
+                    FullData = reader.ReadToEnd().Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+            ParsedData = new double[FullData.Length][];
+            for (int i = 0; i < FullData.Length; i++)
+            {
+                string[] values = FullData[i].Split(',');
+
+                ParsedData[i] = values.Select(s =>
+                {
+                    if (double.TryParse(s, out double result))
+                        return result;
+                    else
+                        throw new FormatException($"Unable to parse '{s}' to double.");
+                }).ToArray();
+            }
+            return ParsedData; // returns all not-null data rows
+        }
+
+
+
+        public static void SaveToCSV(string FilePath, string[] CSVData, bool overwrite = false)
+        {
+            string FileName = Path.GetFileNameWithoutExtension(FilePath);
+            if (!Directory.Exists(Path.GetDirectoryName(FilePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(FilePath));
+            }
+            while (File.Exists(FilePath) && !overwrite)
+            {
+                FileName += " - Copy";
+                FilePath = Path.GetDirectoryName(FilePath) + FileName + ".csv"; //creates a copy of the file
+            }
+            using StreamWriter writer = new StreamWriter(FilePath, false);
+            foreach (string line in CSVData)
+            {
+                writer.WriteLine(line);
+            }
+        }
+    }
+}
--- MergeSorter.cs
+++ MergeSorter.cs
@@ -1,68 +1,125 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DSA
 {
+    /// <summary>
+    /// Static class that contains the .Sort() method to sort the given array. There is a sort function and a sort procedure,
+    /// depending on whether the original array or list needs to be kept or overwritten.
+    /// </summary>
+    /// <typeparam name="T">The data type of the parameters to pass in to use for sorting</typeparam>
     static public class MergeSorter<T> where T : IComparable<T>
     {
-        static public void Sort(T[] source)
+        /// <summary>
+        /// Performs a merge sort on the input array in-place and overwrites the original array with the sorted values
+        /// </summary>
+        /// <param name="Source">The array of values to be sorted</param>
+        static public void Sort(T[] Source)
         {
-            int n = source.Length;
+            int n = Source.Length; //stores array length
 
-            for (int CurrentSize = 1; CurrentSize < n; CurrentSize *= 2)
+            for (int CurrentSize = 1; CurrentSize < n; CurrentSize *= 2) //iterator that specifies what the current sublist size is (powers of 2 ascending)
             {
-                for (int Left = 0; Left < n - 1; Left += 2 * CurrentSize)
+                for (int Left = 0; Left < n - 1; Left += 2 * CurrentSize) //iterator to go through every "Left" index based on the current size, to get the range of 2 arrays to merge
                 {
                     int Mid = Math.Min(Left + CurrentSize - 1, n - 1);
+                    //middle is either Left + CurrentSize - 1, or (n - 1), whichever is smaller - to prevent indexing out of bounds errors
                     int Right = Math.Min(Left + 2 * CurrentSize - 1, n - 1);
-                    MergeArrays(source, Left, Mid, Right);
+                    //right is either Left + CurrentSize * 2 - 1 or (n - 1), whichever is smaller, as input arrays will not always be lengths of powers of 2, the array at the end will be of an odd length
+                    MergeArrays(Source, Left, Mid, Right); //merging the two subarrays inplace back into the Destination array into the correct index range
                 }
             }
             return;
         }
-
-        static private void MergeArrays(T[] source, int left, int mid, int right)
+        /// <summary>
+        /// Performs a merge sort on the input list in-place and overwrites the original list with the sorted values
+        /// </summary>
+        /// <param name="Source">The list of values to be sorted</param>
+        static public void Sort(List<T> Source)
         {
-            int LeftSize = mid - left + 1;
-            int RightSize = right - mid;
+            int n = Source.Count;
+            T[] SourceArray = Source.ToArray();
+            for (int CurrentSize = 1; CurrentSize < n; CurrentSize *= 2) //iterator that specifies what the current sublist size is (powers of 2 ascending)
+            {
+                for (int Left = 0; Left < n - 1; Left += 2 * CurrentSize) //iterator to go through every "Left" index based on the current size, to get the range of 2 arrays to merge
+                {
+                    int Mid = Math.Min(Left + CurrentSize - 1, n - 1);
+                    //middle is either Left + CurrentSize - 1, or (n - 1), whichever is smaller - to prevent indexing out of bounds errors
+                    int Right = Math.Min(Left + 2 * CurrentSize - 1, n - 1);
+                    //right is either Left + CurrentSize * 2 - 1 or (n - 1), whichever is smaller, as input arrays will not always be lengths of powers of 2, the array at the end will be of an odd length
+                    MergeArrays(SourceArray, Left, Mid, Right); //merging the two subarrays inplace back into the Destination array into the correct index range
+                }
+            }
+            Source = SourceArray.ToList<T>(); //the sorted array is converted back to a list and overwrites the original source list
+            return;
+        }
+        /// <summary>
+        /// Performs a merge sort on the input array and returns a sorted copy of the input array without modifying the input array
+        /// </summary>
+        /// <param name="Source">The array of values to be sorted</param>
+        /// <returns>Sorted copy of the input array</returns>
+        static public T[] SortAsCopy(T[] Source)
+        {
+            int n = Source.Length; //stores the array length
+            T[] Destination = new T[Source.Length]; //initialises a destination array for the sorted data
+            Array.Copy(Source, Destination, n); //creates a copy of the source array to use for sorting
+
+
+            for (int CurrentSize = 1; CurrentSize < n; CurrentSize *= 2) //iterator that specifies what the current sublist size is (powers of 2 ascending)
+            {
+                for (int Left = 0; Left < n - 1; Left += 2 * CurrentSize) //iterator to go through every "Left" index based on the current size, to get the range of 2 arrays to merge
+                {
+                    int Mid = Math.Min(Left + CurrentSize - 1, n - 1);
+                    //middle is either Left + CurrentSize - 1, or (n - 1), whichever is smaller - to prevent indexing out of bounds errors
+                    int Right = Math.Min(Left + 2 * CurrentSize - 1, n - 1);
+                    //right is either Left + CurrentSize * 2 - 1 or (n - 1), whichever is smaller, as input arrays will not always be lengths of powers of 2, the array at the end will be of an odd length
+                    MergeArrays(Destination, Left, Mid, Right); //merging the two subarrays inplace back into the Destination array into the correct index range
+                }
+            }
+            return Destination; //the sorted copy of the unsorted Source array is returned
+        }
+
+        static private void MergeArrays(T[] Source, int LeftIndex, int Middle, int RightIndex)
+        {
+            //Left array is the range of index Left to Mid
+            //Right array is the range of index (Mid + 1) to Right
+            int LeftSize = Middle - LeftIndex + 1;
+            int RightSize = RightIndex - Middle;
             T[] Left = new T[LeftSize];
             T[] Right = new T[RightSize];
 
-            for (int i = 0; i < LeftSize; i++)
-            {
-                Left[i] = source[left + i];
-            }
-            for (int i = 0; i < RightSize; i++)
-            {
-                Right[i] = source[mid + 1 + i];
-            }
+            Array.Copy(Source, LeftIndex, Left, 0, LeftSize); //filling the left array from the correct source array range
+            Array.Copy(Source, Middle + 1, Right, 0, RightSize); //filling the right array from the correct source array range
 
-            int pointer1 = 0;
-            int pointer2 = 0;
+            int Pointer1 = 0; //pointer to keep track of the left array's current index during the merging
+            int Pointer2 = 0; //pointer to keep track of the right array's current index during the merging
 
-            for (int i = left; i < (left + LeftSize + RightSize); i++)
+            for (int i = LeftIndex; i < (LeftIndex + LeftSize + RightSize); i++) //iterating through the total data items to merge, starting from the LeftIndex
             {
-                if (pointer1 == LeftSize) //this means all elements used from left array
+                if (Pointer1 == LeftSize) //this means all elements used from left array
                 {
-                    source[i] = Right[pointer2];
-                    pointer2++;
-                    continue;
+                    Source[i] = Right[Pointer2]; 
+                    Pointer2++; //keep filling the passed array with items from the right array until all items used up
+                    continue; //make sure to skip the iteration after this data item is processed
                 }
 
-                if (pointer2 == RightSize) //this means all elements used from right array
+                if (Pointer2 == RightSize) //this means all elements used from right array
                 {
-                    source[i] = Left[pointer1];
-                    pointer1++;
-                    continue;
+                    Source[i] = Left[Pointer1];
+                    Pointer1++; //keep filling the passed array with items from the left array until all items used up
+                    continue; //make sure to skip the iteration after this data item is processed
                 }
 
-                if (Left[pointer1].CompareTo(Right[pointer2]) < 0) //left item less than right item
+                if (Left[Pointer1].CompareTo(Right[Pointer2]) < 0) //left item is less than right item
                 {
-                    source[i] = Left[pointer1];
-                    pointer1++;
+                    Source[i] = Left[Pointer1];
+                    Pointer1++; //increment the left array's pointer
                 }
-                else //right less than left
+                else //right item is less than left item
                 {
-                    source[i] = Right[pointer2];
-                    pointer2++;
+                    Source[i] = Right[Pointer2];
+                    Pointer2++; //increment the right array's pointer
                 }
             }
             return;
